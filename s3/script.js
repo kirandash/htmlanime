7cf2168bb2c5aac608447504d90717f27ed656da
kirandash <thekirankumardash@gmail.com>
2017-02-24 22:05:18 +0530

    S3 - 2 Use DOM to animate objects

--- s3/script.js
+++ s3/script.js
@@ -13,11 +13,29 @@
 
 	$.addEventListener("DOMContentLoaded", function(){
 		//Set up a conventional Rendering Screen
-		var body = $.getElementByTagName('body')[0];
+		var body = $.getElementsByTagName('body')[0],
+			obj = $.createElement('div'),
+			x = 0,
+			y = 0,
+			vx = 4;
+
+		obj.style.width = 100 + 'px';
+		obj.style.height = 100 + 'px';
+		obj.style.backgroundColor = '#F00';
+
+		obj.style.position = 'absolute';
+
+		body.appendChild(obj);
 
 		update();
 
 		function update() {
+			if((x + 100) > $.body.clientWidth || x < 0) {
+				vx *= -1;
+			}
+			x += vx;
+			obj.style.left = x + 'px';
+			obj.style.top = y + 'px';
 			requestAnimFrame(update); //For equivalent of setTimeout or setInterval - like c, c++ -- continuously call update
 		}
 
