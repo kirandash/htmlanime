d490481a515067c0730bb5be159effdcd28b7a5b
kirandash <thekirankumardash@gmail.com>
2017-02-26 11:56:17 +0530

    S5 - 2 Animating spritesheet

--- s5/script.js
+++ s5/script.js
@@ -14,7 +14,11 @@
 		var body = $.getElementsByTagName('body')[0],
 			canvas = $.createElement('canvas'),
 			c = canvas.getContext('2d'),
-			img = new Image();
+			img = new Image(),
+			maxFrames = 12,
+			width = 153,
+			height = 139,
+			currentPosition = 0;
 			
 		img.src = 'fox.png';
 		img.addEventListener('load', update, false);
@@ -26,11 +30,20 @@
 		
 		function update() {
 			
+			if(currentPosition === maxFrames-1){
+				//reset current position
+				currentPosition = 0;
+			}
+
+			currentPosition++;
+
 			c.clearRect(0, 0, canvas.width, canvas.height); //Clear the drawn image to avoid pixelated layers
 			
-			c.drawImage(img, 0, 0);
+			c.drawImage(img, width * currentPosition, 0, width, height, 0, 0, width, height);//position and dimension and offset
 			
-			requestAnimFrame(update);
+			requestAnimFrame(update); //Default 60 frames per second
+
+			//setTimeout(update, 100); //Temporary replacement for animframe
 
 		}
 		
